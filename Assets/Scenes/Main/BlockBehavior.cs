883b0d455de9f8600bfb91149567a933de732246
muraikenta <muraikenta0507@gmail.com>
2021-12-06 02:51:41 +0900

    ブロック壊したときのエフェクト追加

--- Assets/Scenes/Main/BlockBehavior.cs
+++ Assets/Scenes/Main/BlockBehavior.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class BlockBehavior : MonoBehaviour
 {
+    [SerializeField] GameObject effect;
     private void OnCollisionEnter(Collision collision)
     {
+        Instantiate(effect, transform.position, Quaternion.identity);
     }
 }
